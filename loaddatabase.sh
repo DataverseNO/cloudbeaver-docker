cd79b6a6c8ea3c7f138ea09ad8cc7c1b55d7acb3
louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-07-07 08:44:07 +0000

    upaded traefik and loaddatabase.sh

--- loaddatabase.sh
+++ loaddatabase.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-gzip -cd  /tmp/dataverse.dump.gz > /mnt/tmp/postgres/dataverse.dump
+DBZA=/tmp/dataverse.dump.gz
+if [ -f "$DBZA" ]; then
+	    gzip -cd  $DBZA > /mnt/tmp/postgres/dataverse.dump
+fi
+
+chmod 755 /mnt/tmp/postgres/dataverse.dump
+docker stop cloudbeaver
 docker exec --user postgres postgres dropdb -U dataverse dataverse
 docker exec --user postgres postgres createdb -U dataverse dataverse
 docker exec --user postgres postgres  psql -U dataverse dataverse -f /mnttmp/dataverse.dump
+docker start cloudbeaver
+
+
+#0 3 * * * bash /distrib/cloudbeaver-docker/loaddatabase.sh 
+
