ae7afde07a0391275d9d9c1bcc8fdb980ffdb43a
Benedikt Kruse <149382667+BenediktMeierUIT@users.noreply.github.com>
2025-01-31 11:42:10 +0100

    Update loaddatabase.sh

--- loaddatabase.sh
+++ loaddatabase.sh
@@ -9,6 +9,7 @@ docker stop cloudbeaver
 docker exec --user postgres postgres dropdb -U dataverse dataverse
 docker exec --user postgres postgres createdb -U dataverse dataverse
 docker exec --user postgres postgres  psql -U dataverse dataverse -f /mnttmp/dataverse.dump
+docker exec --user postgres postgres  psql -U dataverse dataverse -f /scripts/delete_private_keys.sql
 docker start cloudbeaver
 
 
