f6a47a207c4ee7ded2b7d9a847f7ac4958716c04
Benedikt Kruse <149382667+BenediktMeierUIT@users.noreply.github.com>
2025-01-31 11:41:59 +0100

    Create delete_private_keys.sql

---
+++ scripts/delete_private_keys.sql
@@ -0,0 +1,6 @@
+DELETE FROM public.actionlogrecord;
+DELETE FROM public.oauth2tokendata;
+DELETE FROM public.authenticationproviderrow;
+DELETE FROM public.authenticateduser;
+DELETE FROM public.builtinuser;
+DELETE FROM public.guestbookresponse;
